b614583d2ada54e0808d94b7f951c2c98586188e
Tayo <ebodaeyitayo@gmail.com>
2026-05-07 09:33:26 -0400

    Fix Firebase config

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -3,12 +3,14 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey:            import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain:        import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
-  projectId:         import.meta.env.VITE_FIREBASE_PROJECT_ID,
-  storageBucket:     import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
-  appId:             import.meta.env.VITE_FIREBASE_APP_ID,
+  apiKey: "AIzaSyCqDNzaNqjMNY8QSyiHmgKSXvZ7DaR-KCg",
+  authDomain: "cellfinder-17a3e.firebaseapp.com",
+  databaseURL: "https://cellfinder-17a3e-default-rtdb.firebaseio.com",
+  projectId: "cellfinder-17a3e",
+  storageBucket: "cellfinder-17a3e.firebasestorage.app",
+  messagingSenderId: "23857370822",
+  appId: "1:23857370822:web:72810814129d3a496c69c7",
+  measurementId: "G-N4853MMGF0"
 };
 
 const app  = initializeApp(firebaseConfig);
